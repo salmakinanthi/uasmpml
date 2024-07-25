3a205aac8fb26e9fe2227b3fc7b19fc3f9d17125
salmakinanthi <22611025@students.uii.ac.id>
2024-07-25 12:20:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import pandas as pd
 from io import BytesIO
 
 # URLs to the files on GitHub
-MODEL_URL = "https://github.com/salmakinanthi/uasmpml/raw/master/files/model.pkl"
-ENCODERS_URL = "https://github.com/salmakinanthi/uasmpml/raw/master/files/label_encoders.pkl"
+MODEL_URL = "https://raw.githubusercontent.com/salmakinanthi/uasmpml/master/app.py"
+ENCODERS_URL = "https://raw.githubusercontent.com/salmakinanthi/uasmpml/master/label_encoders.pkl"
 
 def load_file_from_url(url):
     try:
